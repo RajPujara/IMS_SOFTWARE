6e619409ac942e311c1c92a34e53cda5d1af4060
rajpujara <raj.pujara480@gmail.com>
2020-03-06 09:13:44 +0545

    Final year project

---
+++ RAJSTORE/UI/frmDeaCust.cs
@@ -0,0 +1,227 @@
+﻿using RAJSTORE.BLL;
+using RAJSTORE.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RAJSTORE.UI
+{
+    public partial class frmDeaCust : Form
+    {
+        public frmDeaCust()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            //Write the code to close this form
+            this.Hide();
+        }
+
+        DeaCustBLL dc = new DeaCustBLL();
+        DeaCustDAL dcDal = new DeaCustDAL();
+
+        userDAL uDal = new userDAL();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Get the Values from Form
+            dc.type = cmbDeaCust.Text;
+            dc.name = txtName.Text;
+            dc.email = txtEmail.Text;
+            dc.contact = txtContact.Text;
+            dc.address = txtAddress.Text;
+            dc.added_date = DateTime.Now;
+            //Getting the ID to Logged in user and passign its value in dealer or cutomer module
+            string loggedUsr = frmLogin.loggedIn;
+            userBLL usr = uDal.GetIDFromUsername(loggedUsr);
+            dc.added_by = usr.id;
+
+            //Creating boolean variable to check whether the dealer or cutomer is added or not
+            bool success = dcDal.Insert(dc);
+
+            if(success==true)
+            {
+                //Dealer or Cutomer inserted successfully 
+                MessageBox.Show("Dealer or Customer Added Successfully");
+                Clear();
+                //Refresh Data Grid View
+                DataTable dt = dcDal.Select();
+                dgvDeaCust.DataSource = dt;
+            }
+            else
+            {
+                //failed to insert dealer or customer
+            }
+        }
+        public void Clear()
+        {
+            txtDeaCustID.Text = "";
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtContact.Text = "";
+            txtAddress.Text = "";
+            txtSearch.Text = "";
+        }
+
+        private void frmDeaCust_Load(object sender, EventArgs e)
+        {
+            //Refresh Data Grid View
+            DataTable dt = dcDal.Select();
+            dgvDeaCust.DataSource = dt;
+        }
+
+        private void dgvDeaCust_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //int variable to get the identityof row clicked
+            int rowIndex = e.RowIndex;
+
+            txtDeaCustID.Text = dgvDeaCust.Rows[rowIndex].Cells[0].Value.ToString();
+            cmbDeaCust.Text = dgvDeaCust.Rows[rowIndex].Cells[1].Value.ToString();
+            txtName.Text = dgvDeaCust.Rows[rowIndex].Cells[2].Value.ToString();
+            txtEmail.Text = dgvDeaCust.Rows[rowIndex].Cells[3].Value.ToString();
+            txtContact.Text = dgvDeaCust.Rows[rowIndex].Cells[4].Value.ToString();
+            txtAddress.Text = dgvDeaCust.Rows[rowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //Get the values from Form
+            dc.id = int.Parse(txtDeaCustID.Text);
+            dc.type = cmbDeaCust.Text;
+            dc.name = txtName.Text;
+            dc.email = txtEmail.Text;
+            dc.contact = txtContact.Text;
+            dc.address = txtAddress.Text;
+            dc.added_date = DateTime.Now;
+            //Getting the ID to Logged in user and passign its value in dealer or cutomer module
+            string loggedUsr = frmLogin.loggedIn;
+            userBLL usr = uDal.GetIDFromUsername(loggedUsr);
+            dc.added_by = usr.id;
+
+            //create boolean variable to check whether the dealer or customer is updated or not
+            bool success = dcDal.Update(dc);
+            
+            if(success==true)
+            {
+                //Dealer and Customer update Successfully
+                MessageBox.Show("Dealer or Customer updated Successfully");
+                Clear();
+                //Refresh the Data Grid View
+                DataTable dt = dcDal.Select();
+                dgvDeaCust.DataSource = dt;
+            }
+            else
+            {
+                //Failed to udate Dealer or Customer
+                MessageBox.Show("Failed to Udpate Dealer or Customer");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Get the id of the user to be deleted from form
+            dc.id = int.Parse(txtDeaCustID.Text);
+
+            //Create boolean variable to check wheteher the dealer or customer is deleted or not
+            bool success = dcDal.Delete(dc);
+
+            if(success==true)
+            {
+                //Dealer or Customer Deleted Successfully
+                MessageBox.Show("Dealer or Customer Deleted Successfully");
+                Clear();
+                //Refresh the Data Grid View
+                DataTable dt = dcDal.Select();
+                dgvDeaCust.DataSource = dt;
+            }
+            else
+            {
+                //Dealer or Customer Failed to Delete
+                MessageBox.Show("Failed to Delete Dealer or Customer");
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //Get the keyowrd from text box
+            string keyword = txtSearch.Text;
+
+            if(keyword!=null)
+            {
+                //Search the Dealer or Customer
+                DataTable dt = dcDal.Search(keyword);
+                dgvDeaCust.DataSource = dt;
+            }
+            else
+            {
+                //Show all the Dealer or Customer
+                DataTable dt = dcDal.Select();
+                dgvDeaCust.DataSource = dt;
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtmail(object sender, EventArgs e)
+        {
+            // email validation start
+            string pattern = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+        @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            if (Regex.IsMatch(txtEmail.Text, pattern))
+            {
+                errorProvider1.Clear();
+            }
+            else
+            {
+                //errorProvider1.SetError(this.txtEmail, "Please provide your valied email");
+                //return;
+                MessageBox.Show(this.txtEmail, "Please provide your valied email", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+
+            }
+            // email validation end
+        }
+
+        private void txtcontact(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 48 && e.KeyChar <= 57) || e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                MessageBox.Show("Please enter only number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                e.Handled = true;
+            }
+        }
+
+        private void txtcontact(object sender, EventArgs e)
+        {
+            if (txtContact.Text.Length < 10 || txtContact.Text.Length > 10)
+            {
+                //MessageBox.Show("Please enter only 10 digit");
+                MessageBox.Show("Please enter only 10 digits.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //txtContact.BackColor = Color.Red;
+                txtContact.Focus();
+
+
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ RAJSTORE/UI/frmUserDashboard.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace RAJSTORE
+{
+    partial class frmUserDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripTop = new System.Windows.Forms.MenuStrip();
+            this.purchaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salesFormsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dealerAndCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblSHead = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblAppFName = new System.Windows.Forms.Label();
+            this.lblLoggedInUser = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStripTop.SuspendLayout();
+            this.pnlFooter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripTop
+            // 
+            this.menuStripTop.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStripTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.purchaseToolStripMenuItem,
+            this.salesFormsToolStripMenuItem,
+            this.inventoryToolStripMenuItem,
+            this.dealerAndCustomerToolStripMenuItem});
+            this.menuStripTop.Location = new System.Drawing.Point(0, 0);
+            this.menuStripTop.Name = "menuStripTop";
+            this.menuStripTop.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStripTop.Size = new System.Drawing.Size(1872, 35);
+            this.menuStripTop.TabIndex = 0;
+            this.menuStripTop.Text = "menuStrip1";
+            // 
+            // purchaseToolStripMenuItem
+            // 
+            this.purchaseToolStripMenuItem.Name = "purchaseToolStripMenuItem";
+            this.purchaseToolStripMenuItem.Size = new System.Drawing.Size(94, 29);
+            this.purchaseToolStripMenuItem.Text = "Purchase";
+            this.purchaseToolStripMenuItem.Click += new System.EventHandler(this.purchaseToolStripMenuItem_Click);
+            // 
+            // salesFormsToolStripMenuItem
+            // 
+            this.salesFormsToolStripMenuItem.Name = "salesFormsToolStripMenuItem";
+            this.salesFormsToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.salesFormsToolStripMenuItem.Text = "Sales";
+            this.salesFormsToolStripMenuItem.Click += new System.EventHandler(this.salesFormsToolStripMenuItem_Click);
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(99, 29);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            this.inventoryToolStripMenuItem.Click += new System.EventHandler(this.inventoryToolStripMenuItem_Click);
+            // 
+            // dealerAndCustomerToolStripMenuItem
+            // 
+            this.dealerAndCustomerToolStripMenuItem.Name = "dealerAndCustomerToolStripMenuItem";
+            this.dealerAndCustomerToolStripMenuItem.Size = new System.Drawing.Size(191, 29);
+            this.dealerAndCustomerToolStripMenuItem.Text = "Dealer and Customer";
+            this.dealerAndCustomerToolStripMenuItem.Click += new System.EventHandler(this.dealerAndCustomerToolStripMenuItem_Click);
+            // 
+            // lblSHead
+            // 
+            this.lblSHead.AutoSize = true;
+            this.lblSHead.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSHead.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.lblSHead.Location = new System.Drawing.Point(820, 549);
+            this.lblSHead.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSHead.Name = "lblSHead";
+            this.lblSHead.Size = new System.Drawing.Size(599, 40);
+            this.lblSHead.TabIndex = 11;
+            this.lblSHead.Text = "Billing and Inventory Management Software";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLName.Location = new System.Drawing.Point(1017, 492);
+            this.lblLName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(149, 55);
+            this.lblLName.TabIndex = 10;
+            this.lblLName.Text = "STORE";
+            // 
+            // lblAppFName
+            // 
+            this.lblAppFName.AutoSize = true;
+            this.lblAppFName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppFName.Location = new System.Drawing.Point(930, 492);
+            this.lblAppFName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAppFName.Name = "lblAppFName";
+            this.lblAppFName.Size = new System.Drawing.Size(92, 55);
+            this.lblAppFName.TabIndex = 9;
+            this.lblAppFName.Text = "RAJ";
+            // 
+            // lblLoggedInUser
+            // 
+            this.lblLoggedInUser.AutoSize = true;
+            this.lblLoggedInUser.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoggedInUser.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblLoggedInUser.Location = new System.Drawing.Point(64, 72);
+            this.lblLoggedInUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLoggedInUser.Name = "lblLoggedInUser";
+            this.lblLoggedInUser.Size = new System.Drawing.Size(0, 28);
+            this.lblLoggedInUser.TabIndex = 8;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(14, 72);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(60, 28);
+            this.lblUser.TabIndex = 7;
+            this.lblUser.Text = "User: ";
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BackColor = System.Drawing.Color.Teal;
+            this.pnlFooter.Controls.Add(this.lblFooter);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 708);
+            this.pnlFooter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(1872, 52);
+            this.pnlFooter.TabIndex = 12;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFooter.ForeColor = System.Drawing.Color.White;
+            this.lblFooter.Location = new System.Drawing.Point(879, 12);
+            this.lblFooter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(228, 28);
+            this.lblFooter.TabIndex = 0;
+            this.lblFooter.Text = "Developed By: Raj Pujara";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1016, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 45);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Logout";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmUserDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1872, 760);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.lblSHead);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblAppFName);
+            this.Controls.Add(this.lblLoggedInUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.menuStripTop);
+            this.MainMenuStrip = this.menuStripTop;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmUserDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmUserDashboard_FormClosed);
+            this.Load += new System.EventHandler(this.frmUserDashboard_Load);
+            this.menuStripTop.ResumeLayout(false);
+            this.menuStripTop.PerformLayout();
+            this.pnlFooter.ResumeLayout(false);
+            this.pnlFooter.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripTop;
+        private System.Windows.Forms.ToolStripMenuItem purchaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salesFormsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.Label lblSHead;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblAppFName;
+        private System.Windows.Forms.Label lblLoggedInUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.Label lblFooter;
+        private System.Windows.Forms.ToolStripMenuItem dealerAndCustomerToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
